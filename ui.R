68b5519e0e351ef8c8a8f500612f020d1c64e66f
Joshua <joshwvs@gmail.com>
2014-08-13 20:02:33 -0400

    Removed tutorial header.

--- ui.R
+++ ui.R
@@ -1,10 +1,3 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-# 
-# http://www.rstudio.com/shiny/
-#
-
 library(shiny)
 
 shinyUI(fluidPage(
