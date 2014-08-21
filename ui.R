91e23683660ce1b880346c597f8ede4cbf1c427c
Joshua Simpson <jwvsimps@uwaterloo.ca>
2014-08-20 20:53:34 -0400

    Switched to navbarPage for ui, added page for histogram.

--- ui.R
+++ ui.R
@@ -1,28 +1,46 @@
 library(shiny)
 
-shinyUI(fluidPage(
+shinyUI(navbarPage("Counting Sheep",
+                    
+  tabPanel("Boxplots", 
+           
+           h3("Counting Sheep: A Year's Worth of Sleep"),
+           
+           plotOutput("main"),
+           
+           hr(),
+           
+           
+           
+           fluidRow(
+             
+             column(5, 
+                    dateRangeInput("dates", "Date Range", 
+                                   start = min(sleep$date), 
+                                   end = max(sleep$date), 
+                                   min = min(sleep$date), 
+                                   max = max(sleep$date))
+                    ),
+             
+             column(7, 
+                    checkboxGroupInput("terms", "Terms to Display", 
+                                       choices = levels(sleep$term), 
+                                       selected = levels(sleep$term), 
+                                       inline = FALSE)
+                    )
+
+            )
+  ),
   
-  h3("Counting Sheep: A Year's Worth of Sleep"),
-  
-  plotOutput("main"),
-  
-  hr(),
-  
-  
-  
-  fluidRow(
-    
-    column(5, 
-    dateRangeInput("dates", "Date Range", 
-                   start = min(sleep$date), end = max(sleep$date), 
-                   min = min(sleep$date), max = max(sleep$date))
-    ),
-    
-    column(7, 
-    checkboxGroupInput("terms", "Terms to Display", 
-                       choices = levels(sleep$term), 
-                       selected = levels(sleep$term), 
-                       inline = FALSE)
-    )
-  )
+  tabPanel("Histogram",
+           
+           plotOutput("main"),
+           
+           hr(),
+           
+           sliderInput("bins", "Bin Size (Hours)", 
+                       min = 0, max = max(sleep$hours),
+                       value = 1, step = 0.5)
+           
+           )
 ))
