d1c6b24a8f7994459da4ca3bd8b1851f6a5e4a08
Joshua Simpson <jwvsimps@uwaterloo.ca>
2014-08-20 20:35:05 -0400

    Added filter for term, updated ui to use fluidRow.

--- server.R
+++ server.R
@@ -1,6 +1,9 @@
-library(shiny, dplyr)
+library(ggplot2)
+library(reshape2)
+library(dplyr)
 
 load("sleep.data.RData")
+
 
 shinyServer(
   function(input, output) {
@@ -10,13 +13,15 @@ shinyServer(
       # filter data based on user selections
       sleep %>% 
         filter(date > input$dates[1], date < input$dates[2]) %>%
+        filter(term %in% input$terms) %>%
         group_by(day) %>%
         mutate(average_hours = mean(hours))
     })
   
   output$main <- renderPlot({
     ggplot(update.sleep(), aes(x = day, y = hours)) +
-      geom_bar(stat="identity")
+      geom_boxplot() +
+      facet_wrap(~term, nrow = 1)
   })
   
   }
--- ui.R
+++ ui.R
@@ -2,19 +2,27 @@ library(shiny)
 
 shinyUI(fluidPage(
   
-  titlePanel("Counting Sheep: A Year's Worth of Sleep"),
+  h3("Counting Sheep: A Year's Worth of Sleep"),
   
-  sidebarLayout(
-    
-    sidebarPanel(
-      
-      dateRangeInput("dates", "Date Range", 
-                     start = min(sleep$date), end = max(sleep$date), 
-                     min = min(sleep$date), max = max(sleep$date))
-      ),
-    
-    mainPanel(
-      plotOutput(outputId = "main")
-    ))
+  plotOutput("main"),
   
-  ))
+  hr(),
+  
+  
+  
+  fluidRow(
+    
+    column(5, 
+    dateRangeInput("dates", "Date Range", 
+                   start = min(sleep$date), end = max(sleep$date), 
+                   min = min(sleep$date), max = max(sleep$date))
+    ),
+    
+    column(7, 
+    checkboxGroupInput("terms", "Terms to Display", 
+                       choices = levels(sleep$term), 
+                       selected = levels(sleep$term), 
+                       inline = FALSE)
+    )
+  )
+))
